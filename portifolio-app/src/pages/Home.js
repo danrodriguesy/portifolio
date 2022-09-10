5594d76b2279e171365d38c6ae5ed96eb5db309c
danrodriguesy <danrodrigues@gmail.com>
2022-09-10 17:17:41 -0300

    ajuste menu mobile

--- portifolio-app/src/pages/Home.js
+++ portifolio-app/src/pages/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { HomeStyle } from '../styles/Home';
+import img from '../image/img-profile.png';
+import {} from 'phosphor-react';
+import { PrimeIcons } from 'primereact/api';
 
 function Home() {
   return (
     <>
-      <div>
-        
-      </div>
+      <HomeStyle>
+        <img src={img} alt="img-profile"/>
+        <h1>Desenvolvedor Front-end</h1>
+        <p>Danrley Rodrigues</p>
+        <div>
+
+        </div>
+      </HomeStyle>
     </>
   )
 }
